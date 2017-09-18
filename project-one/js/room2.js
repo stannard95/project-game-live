e032f9ccbc5ccc3c29fb774d9562f203178df9bd
stannard95 <14066588@brookes.ac.uk>
2017-09-18 19:25:58 +0100

    makeAlert function and finished note puzzle

--- project-one/js/room2.js
+++ project-one/js/room2.js
@@ -2,6 +2,7 @@ $(function () {
 	var $inventory = $('.inventory-box');
 
 	var note = null;
+	var noteboxSelect = null;
 
 	var $noteSelect = $('.noteSelect');
 	$noteSelect.on('click', function (event) {
@@ -10,4 +11,46 @@ $(function () {
 		note.css('border', 'solid yellow 2px');
 	});
 
+	var $noteBoxes = $('.note');
+	$noteBoxes.on('click', function (event) {
+		noteboxSelect = $(this);
+		note.hide();
+
+		// Places note in box
+		if (note !== null) {
+			noteboxSelect.html(note.html());
+			note.css('border', 'none');
+			noteboxSelect.css('background-color', note.css('background-color'));
+		}
+	});
+
+	var $noteButton = $('#noteButton');
+	$noteButton.on('click', function (event) {
+		checkNotesVaild($noteBoxes, $noteSelect);
+	});
+
 });
+
+
+// Checks that the notes have been ordered properly
+function checkNotesVaild(noteBoxes, noteSelect) {
+	if (noteBoxes.eq(4).html() === 'Academy standup' && 
+		noteBoxes.eq(1).html() === 'Sparta day' &&
+		noteBoxes.eq(3).html() === 'Client visit') {
+		makeAlert('Correct! Here is a thing!');
+		
+	} else {
+		noteSelect.show();
+		noteBoxes.html('').css('background-color', 'brown');
+		makeAlert('Incorrect! Try again!');
+
+	} 
+}
+
+// Makes an alert from the text given
+function makeAlert (text) {
+	alert(text);
+}
+
+
+
